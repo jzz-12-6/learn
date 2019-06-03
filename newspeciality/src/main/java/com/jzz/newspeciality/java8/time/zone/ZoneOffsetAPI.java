859dc1048320bbdb0cd8f7ec318f1e899c9633c4
jingzezhong <jjinzezhong@gmail.com>
2019-06-03 22:17:55 +0800

    java8新特效 ZoneOffset

--- newspeciality/src/main/java/com/jzz/newspeciality/java8/time/zone/ZoneOffsetAPI.java
+++ newspeciality/src/main/java/com/jzz/newspeciality/java8/time/zone/ZoneOffsetAPI.java
@@ -1,12 +1,19 @@
 package com.jzz.newspeciality.java8.time.zone;
 
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoField;
+
+import static java.time.temporal.ChronoField.OFFSET_SECONDS;
 
 /**
+ * 类表示格林威治/UTC的时区偏移量
  * @author jzz
  * @date 2019/6/3
  */
 public class ZoneOffsetAPI {
+    static   ZoneOffset offset = ZoneOffset.ofHours(5);
+
     public static void main(String[] args) {
         // 创建ZoneOffset
         /**
@@ -30,6 +37,38 @@ public class ZoneOffsetAPI {
          * +00:01:40
          */
         ZoneOffset.ofTotalSeconds(100);
-
+        /**
+         * +08:00
+         */
+        ZoneOffset.from(ZonedDateTime.now());
+        /**
+         * 18000
+         */
+        ZoneOffset.ofHours(5).getTotalSeconds();
+        /**
+         * ZoneRules[currentStandardOffset=+05:00]
+         */
+        ZoneOffset.ofHours(5).getRules();
+        /**
+         * 是否支持指定的字段
+         * true
+         */
+        offset.isSupported(ChronoField.OFFSET_SECONDS);
+        /**
+         * 获取指定字段的有效值范围
+         * -64800 - 64800
+         */
+        offset.range(ChronoField.OFFSET_SECONDS);
+        /**
+         * 获取指定字段的值
+         * 18000
+         */
+        offset.get(OFFSET_SECONDS);
+        offset.getLong(OFFSET_SECONDS);
+        /**
+         * 调整指定时态对象以具有此ZoneOffset。
+         * 2019-06-03T22:13:23.002538800+08:00[Asia/Shanghai]x
+         */
+        offset.adjustInto(ZonedDateTime.now());
     }
 }
