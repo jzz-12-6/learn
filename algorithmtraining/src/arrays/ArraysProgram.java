eddb48d4d5c9ede61e08baf38c83e4d2c02dec35
jzz-12-6 <jzz110120@163.com>
2019-09-04 18:06:47 +0800

    1.netty

--- algorithmtraining/src/arrays/ArraysProgram.java
+++ algorithmtraining/src/arrays/ArraysProgram.java
@@ -15,8 +15,84 @@ public class ArraysProgram {
 
 
     public static void main(String[] args) {
-        prefixesDivBy5(new int[]{1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,1,0}) ;
+        findRestaurant(new String[]{"Shogun", "Tapioca Express", "Burger King", "KFC"},new String[]{"KFC", "Shogun", "Burger King"});
     }
+
+
+    /**
+     * 599. 两个列表的最小索引总和
+     * 假设Andy和Doris想在晚餐时选择一家餐厅，并且他们都有一个表示最喜爱餐厅的列表，每个餐厅的名字用字符串表示。
+     *
+     * 你需要帮助他们用最少的索引和找出他们共同喜爱的餐厅。 如果答案不止一个，则输出所有答案并且不考虑顺序。 你可以假设总是存在一个答案。
+     *
+     输入:
+     ["Shogun", "Tapioca Express", "Burger King", "KFC"]
+     ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"]
+     输出: ["Shogun"]
+     解释: 他们唯一共同喜爱的餐厅是“Shogun”。
+     */
+    public static String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map1 = new HashMap<>(list1.length);
+        for (int i = 0; i < list1.length; i++) {
+            map1.put(list1[i],i);
+        }
+        //获取重复的元素，并储存下标之和
+        Map<String, Integer> result = new HashMap<>(16);
+        for (int i = 0; i < list2.length; i++) {
+            Integer j = map1.get(list2[i]);
+            if (j !=null){
+                result.put(list2[i],j+i);
+            }
+        }
+        //获取一个最小下标和
+        Set<String> set = result.keySet();
+        String minKey ="" ;
+        for (String s : set) {
+            minKey = s;
+            break;
+        }
+        int min = result.get(minKey);
+        Set<Map.Entry<String, Integer>> entries = result.entrySet();
+        List<String> list = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : entries) {
+            Integer v = entry.getValue();
+            if(v < min){
+                list.clear();
+                list.add(entry.getKey());
+                min = v;
+            }else if(v == min){
+                list.add(entry.getKey());
+            }
+        }
+        return   list.toArray(new String[0]);
+    }
+
+    /**
+     * 169. 求众数
+     * 给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+     *
+     * 你可以假设数组是非空的，并且给定的数组总是存在众数。
+     * 输入: [3,2,3]
+     * 输出: 3
+     * @param nums
+     * @return
+     */
+    public static int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>(16);
+        for (int num : nums) {
+            map.merge(num,1,(v1,v2)->v1+v2);
+        }
+        int i = nums.length / 2;
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            Integer value = entry.getValue();
+            if(value > i){
+                return entry.getKey();
+            }
+        }
+        return 1;
+    }
+
     //[false,false,true,false,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,true,false,false,true,false,false,true,true,true,true,true,true,true,false,false,true,true,false,false,false,false,false]
     //[false,false,true,false,false,false,false,false,false,false,true,true,true,true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,true,false,false,true,false,false,true,true,true,true,true,true,true,false,false,true,false,false,false,false,true,true]
     /**
